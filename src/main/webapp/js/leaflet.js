bdf9be1fae6916b9fb357a3301b6fd277b7d3ab1
Philippe Deslongchamps <philippe.deslongchamps@gmail.com>
2016-07-08 02:27:02 -0400

    commit

--- src/main/webapp/js/leaflet.js
+++ src/main/webapp/js/leaflet.js
@@ -7,7 +7,7 @@ var markers;
 
 function initMap(){
     markers = new L.MarkerClusterGroup();
-    map = L.map('mapId').setView([45.52, -73.66], 12);
+    map = L.map('mapContainer').setView([45.52, -73.66], 12);
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
